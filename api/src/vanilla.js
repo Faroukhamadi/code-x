3fd7ec6b0357451ca9c8cd62550b839e5aed8689
faroukhamadi <faroukhamadi1@outlook.com>
2024-02-28 17:39:35 +0100

    change vanilla code

--- api/src/vanilla.js
+++ api/src/vanilla.js
@@ -1,11 +1,18 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-	if (req.method === 'GET' && req.url === '/') {
+const server = http.createServer(async (req, res) => {
+	if (req.url === '/' && req.method === 'GET') {
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.write(JSON.stringify({ message: 'hello' }));
+
 		res.end();
+		return;
 	}
+
+	res.writeHead(404, { 'Content-Type': 'application/json' });
+	res.end(JSON.stringify({ message: 'nope' }));
 });
 
 server.listen(42069, () => {
-	console.log('server on 42069');
+	console.log('listening to server');
 });
